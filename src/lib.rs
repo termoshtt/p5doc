6098393ae5d062c323bf80f748bbe5c891c110c0
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-03-07 16:58:26 +0900

    Init

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+/// Some function!
+///
+/// Before
+///
+/// <script src="https://cdn.jsdelivr.net/npm/p5@1.6.0/lib/p5.js"></script>
+/// <script>
+/// function setup() {
+///   var canvas = createCanvas(200, 200);
+///   canvas.parent("doc-some");
+/// }
+/// function draw() {
+///   background(220);
+///   ellipse(50,50,80,80);
+/// }
+/// </script>
+/// <div id="doc-some"></div>
+///
+/// After
+///
+pub fn some() {}
